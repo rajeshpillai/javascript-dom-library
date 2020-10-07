f8e40c4faf2df32c7516c4ffe788bd5e66e3d939
Rajesh Pillai <pillai.rajesh@gmail.com>
2020-10-07 17:28:53 +0530

    Delete todo with jquery fadeOut animation

--- app/views/todos.js
+++ app/views/todos.js
@@ -40,6 +40,25 @@ $("#btnAdd").on("click", (e) => {
 });
 
 
+//BEST PRACTICE:  Optimized way to attach events to a list of elements
+$("#tasks").on("click", ".ui-delete", (e) => {
+  
+  //alert(e.target.parentNode.parentNode.getAttribute("data-task-id"));
+  let taskId = $(e.target).closest("button").attr("data-task-id");
+  TodoData = TodoData.filter(t => t.id !== taskId);
+  
+  let response = window.confirm("Do you want to delete this todo?");
+
+  let currentTaskItem$ = $(`#task-${taskId}`);
+
+  if (response) {
+    currentTaskItem$.fadeOut("slow", ()=> {
+      currentTaskItem$.remove();
+    }); 
+  }
+});
+
+
 // Render full task 
 var tasksUI =  function () {
 
@@ -58,9 +77,17 @@ var taskItemUI = function (task) {
 
   return (
     `
-      <div class="card mt-3" >
+      <div class="card mt-3" id=task-${task.id} >
         <div class="card-body">
-          <h5 class="card-title">${task.title}</h5>
+          <div class="d-flex justify-content-between">
+            <h5 class="card-title">${task.title}</h5>
+            <button data-task-id =${task.id} class="btn btn-outline ui-delete">
+              <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+               <path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z"/>
+              </svg>
+            </button>
+          </div>
+
           <span class="badge ${statusClass}">
             ${task.completed ? "completed": "in-progress" }
           </span>
